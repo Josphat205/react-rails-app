1e8028a7e56c6133176094104b6ac32337154bc4
jloman <jloman200@gmail.com>
2023-05-04 19:41:25 +0300

    Added routes in the app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Greetings from "./components/Greetings";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store";
 function App() {
   return (
     <div>
-      <Greetings />
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Greetings />} />
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
