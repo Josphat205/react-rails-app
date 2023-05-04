03f70ef81f704ef8b737ab4643ff8e2d5f2f21eb
jloman <jloman200@gmail.com>
2023-05-04 11:35:51 +0300

    Add rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'root#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,5 +8,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   # Defines the articles path route ("/articles")
-    get "/greetings", to: "greetings#say_hello"
+  get '/greetings', to: 'greetings#say_hello'
 end
